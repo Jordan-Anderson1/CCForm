2225c4042b17d24a48f89ac55c57f8d8b099ee3f
jordan <andersonjor@deakin.edu.au>
2024-06-03 20:30:40 +1000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import CardFront from '/CardFront'
-import CardBack from '/CardBack'
+import CardFront from './CardFront'
+import CardBack from './CardBack'
 import Form from './Form'
 import Thankyou from '/Thankyou'
 import './App.css'
