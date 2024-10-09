4e7bf5be6fa041a01827d06b62fb2ca9b4f59a83
Jordan <andersonjor@deakin.edu.au>
2024-10-09 15:16:31 +1100

    card displays with spcaes in number

--- src/CardFront.jsx
+++ src/CardFront.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const CardFront = ({cardNumber, name, date, month, className}) => {
+const CardFront = ({ cardNumber, name, date, month, className }) => {
   return (
-    <div className={`absolute ${className}`} >
-        <img src="/bg-card-front.png" />
-        <img 
-            className='absolute left-6 top-6'
-        src='/card-logo.svg'/>
+    <div className={`absolute ${className}`}>
+      <img src="/bg-card-front.png" />
+      <img className="absolute left-6 top-6" src="/card-logo.svg" />
 
-        <p className='absolute left-6 top-28 text-2xl text-white'>{`${cardNumber === '' ? '0000 0000 0000 0000' : cardNumber}`}</p>
-        <p className='absolute bottom-6 left-6 text-white text-md uppercase'>{`${name === '' ? 'Jane Appleseed' : name}`}</p>
-        <p className='absolute bottom-6 right-6 text-white text-md'>{`${date === '' ? '00' : date}/${month === '' ? '00' : month}`}</p>
-
-        
+      <p className="absolute left-6 top-28 text-2xl text-white">{`${
+        cardNumber === ""
+          ? "0000 0000 0000 0000"
+          : cardNumber
+              .toString()
+              .match(/.{1,4}/g)
+              .join(" ")
+      }`}</p>
+      <p className="absolute bottom-6 left-6 text-white text-md uppercase">{`${
+        name === "" ? "Jane Appleseed" : name
+      }`}</p>
+      <p className="absolute bottom-6 right-6 text-white text-md">{`${
+        date === "" ? "00" : date
+      }/${month === "" ? "00" : month}`}</p>
     </div>
-  )
-}
+  );
+};
 
-export default CardFront
+export default CardFront;
